e480ef12966d602c63f8e12eda501883cf383921
anujanegi <anujanegi1@gmail.com>
2022-05-31 14:07:37 +0200

    monitor data viz #5

--- visualise.py
+++ visualise.py
@@ -36,7 +36,7 @@ def plot_stimulus(stimulus, position, type=""):
     if type == "iTBS":
         plt.imshow(stimulus(), interpolation="none", aspect="auto")
         plt.xlabel("Time")
-        plt.title("%s Coil Position"%position)
+        plt.title("%s Coil Position" % position)
         plt.ylabel("Space")
         plt.colorbar()
         plt.show()
@@ -47,4 +47,57 @@ def plot_stimulus(stimulus, position, type=""):
         fig.set_size_inches(6, 5)
         fig.suptitle("%s Coil Position" % position, fontsize=24, y=1.05)
         fig.tight_layout()
+        plt.show()
+
+
+def plot_monitor_data(monitor_data, monitor_list, EOI=[3, 41, 42, 58], EOI_labels=None):
+    if EOI_labels is None:
+        EOI_labels = [str(i) for i in EOI]
+
+    if "tavg" in monitor_list:
+        tavg_time, TAVG = monitor_data["tavg"]["time"], monitor_data["tavg"]["data"]
+
+        plt.figure(figsize=(11, 3))
+        plt.plot(tavg_time, TAVG[:, 0, :, 0], "k", alpha=0.1)
+        plt.plot(tavg_time, TAVG[:, 0, :, 0].mean(axis=1), "r", alpha=1)
+        plt.title("Temporal average")
+        plt.xlabel("Time (ms)")
+        plt.axis([1000, len(tavg_time), -1.2, 2.4])
+        plt.axvspan(1500, len(tavg_time), color="whitesmoke")  # stimuli span
+        plt.show()
+
+    elif "savg" in monitor_list:
+        savg_time, SAVG = monitor_data["savg"]["time"], monitor_data["savg"]["data"]
+
+        plt.figure(figsize=(11, 3))
+        plt.plot(savg_time, SAVG[:, 0, :, 0])
+        plt.title("Spatial average")
+        plt.xlabel("Time (ms)")
+        plt.xlim(1000, len(tavg_time))
+        plt.axvspan(1500, len(tavg_time), color="whitesmoke")  # stimuli span
+        plt.show()
+
+    elif "eeg" in monitor_list:
+        eeg_time, EEG = monitor_data["eeg"]["time"], monitor_data["eeg"]["data"]
+
+        plt.figure(figsize=(11, 3))
+        plt.plot(eeg_time, EEG[:, 0, :, 0], "k", alpha=0.1)
+        plt.plot(eeg_time, EEG[:, 0, EOI, 0], alpha=1, label=EOI_labels)  # EOIs
+        plt.title("EEG")
+        plt.xlabel("Time (ms)")
+        plt.xlim(1000, len(tavg_time))
+        plt.axvspan(1500, len(tavg_time), color="whitesmoke")  # stimuli span
+        plt.legend()
+        plt.show()
+
+    elif "bold" in monitor_list:
+        bold_time, BOLD = monitor_data["bold"]["time"], monitor_data["bold"]["data"]
+
+        plt.figure(figsize=(11, 3))
+        plt.plot(bold_time, BOLD[:, 0, :, 0], "k", alpha=0.1)
+        plt.plot(bold_time, BOLD[:, 0, EOI, 0], alpha=1, label=EOI_labels)  # EOIs
+        plt.title("BOLD")
+        plt.xlim(1000, len(tavg_time))
+        plt.axvspan(1500, len(tavg_time), color="whitesmoke")  # stimuli span
+        plt.legend()
         plt.show()
