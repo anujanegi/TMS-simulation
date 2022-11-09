9e7e65186d9788ccd808f01bdfc3024b09993fa0
anujanegi <anujanegi1@gmail.com>
2022-11-09 12:09:29 +0100

    support for jansen rit and rWW  NMM

--- simulation.py
+++ simulation.py
@@ -16,22 +16,59 @@ from tvb.simulator.lab import (
 import numpy as np
 
 
-def get_brain_models(dt=2 ** -6):
-    # Define the model
-    neuron_model = models.Generic2dOscillator(a=np.array([0.3]), tau=np.array([2]))
+def get_brain_models(
+    NMM="oscillator",
+    path_to_SC=None,
+    path_to_tract_lenghts=None,
+    path_to_region_labels=None,
+    dt=2 ** -6,
+):
+    # TODO: docstring
+    """Get the brain models for the simulation."""
+    # Define the neural mass model
+    if NMM == "oscillator":
+        neuron_model = models.Generic2dOscillator(a=np.array([0.3]), tau=np.array([2]))
+        nsig = np.array([5e-5])
+    elif NMM == "reduced_wong_wang":
+        neuron_model = models.ReducedWongWang(
+            a=np.array([0.27]), w=np.array([1.0]), I_o=np.array([0.3])
+        )
+        nsig = np.array([1e-5])
+    elif NMM == "jansen_rit":
+        neuron_model = models.JansenRit(mu=np.array([0.0]), v0=np.array([6.0]))
+        phi_n_scaling = (
+            neuron_model.a
+            * neuron_model.A
+            * (neuron_model.p_max - neuron_model.p_min)
+            * 0.5
+        ) ** 2 / 2.0
+        nsig = np.zeros(6)
+        nsig[3] = phi_n_scaling
 
     # Define the connectivity between regions
-    white_matter = connectivity.Connectivity.from_file()
-    white_matter.speed = np.array([4.0])
+    if path_to_SC and path_to_region_labels and path_to_tract_lenghts:
+        white_matter = connectivity.Connectivity(
+            tract_lengths=np.loadtxt(path_to_tract_lenghts),
+            weights=np.loadtxt(path_to_SC),
+            region_labels=np.loadtxt(path_to_region_labels, dtype=str),
+            centres=np.array([0]),
+        )
+    else:
+        white_matter = connectivity.Connectivity.from_file()
+        white_matter.speed = np.array([4.0])
     white_matter.configure()
 
     # Define the coupling
-    white_matter_coupling = coupling.Difference(a=np.array([7e-4]))
+    if NMM == "oscillator":
+        white_matter_coupling = coupling.Difference(a=np.array([7e-4]))
+    elif NMM == "reduced_wong_wang":
+        coupling.Linear(a=np.array([0.5 / 50.0])),
+    elif NMM == "jansen_rit":
+        white_matter_coupling = coupling.SigmoidalJansenRit(a=np.array([0.0]))
+    white_matter_coupling.configure()
 
     # Define an integration method
-    heunint = integrators.HeunStochastic(
-        dt=dt, noise=noise.Additive(nsig=np.array([5e-5]))
-    )
+    heunint = integrators.HeunStochastic(dt=dt, noise=noise.Additive(nsig=nsig))
 
     # Define a cortical surface
     default_cortex = cortex.Cortex.from_file()
