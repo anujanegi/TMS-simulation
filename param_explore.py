8743f8a1920ae79a82d19f6c1a140c63a10a3e3c
anujanegi <anujanegi1@gmail.com>
2023-04-11 11:40:47 +0200

    parameter exploration

--- param_explore.py
+++ param_explore.py
@@ -6,35 +6,52 @@ from itertools import repeat
 from run_simulations import run_TMS_EEG_simulations
 
 
-def simulation_run_for_all_subjects(global_coupling=0.0):
+def simulation_run_for_all_subjects(global_coupling=0.0, type=None):
     do_resting_state_simulation = False
-    overwrite = True
+    overwrite = False
     plotting = True
 
-    for type in config.subjects:
-        for subject in config.subjects[type]:
-            run_TMS_EEG_simulations(
-                subject=subject,
-                type=type,
-                overwrite=overwrite,
-                do_resting_state_simulation=do_resting_state_simulation,
-                efield_type="individual",
-                global_coupling=global_coupling,
-                plotting=plotting,
-            )
+    if type is None:
+        print("Please specify Subject type for simulation")
+        return
+    
+    for subject in config.subjects[type]:
+        run_TMS_EEG_simulations(
+            subject=subject,
+            type=type,
+            overwrite=overwrite,
+            do_resting_state_simulation=do_resting_state_simulation,
+            efield_type="individual",
+            global_coupling=global_coupling,
+            plotting=plotting,
+        )
 
 
 if __name__ == "__main__":
     list_of_args = sys.argv[1:]
 
     if "explore_GC" in list_of_args:
-
-        n_cores = 4
-        p = mp.Pool(processes=n_cores)
+        
         gc_range = np.arange(0.0001, 1, 0.025)
-        p.map(simulation_run_for_all_subjects, gc_range)
-        p.close()
 
+        # do AD
+        n_cores = 2
+        p = mp.Pool(processes=n_cores)
+        p.starmap(simulation_run_for_all_subjects, zip(gc_range, repeat("AD")))
+        p.close()
+        
+        # do MCI
+        n_cores = 2
+        p = mp.Pool(processes=n_cores)
+        p.starmap(simulation_run_for_all_subjects, zip(gc_range, repeat("MCI")))
+        p.close()
+        
+        # do HC
+        n_cores = 2
+        p = mp.Pool(processes=n_cores)
+        p.starmap(simulation_run_for_all_subjects, zip(gc_range, repeat("HC")))
+        p.close()
+        
     else:
         print("Supported arguments:")
         print("explore_GC: explore global coupling values")
