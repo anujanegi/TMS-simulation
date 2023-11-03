6bcb04654f3a0c0718a96f8163c69aa6d0491d32
anujanegi <anujanegi1@gmail.com>
2023-11-03 12:39:46 +0100

    check if dir exists

--- utils/charm_all_subjects.py
+++ utils/charm_all_subjects.py
@@ -9,12 +9,14 @@ def main():
     # run command 'charm' on all subjects in confif.subjects
     for subject_type in config.subjects:
         for subject in config.subjects[subject_type]:
-            print(f"Running charm on {subject}")
             os.chdir(config.get_subject_path(subject, subject_type))
-            os.system(
-                f"charm {subject[-4:]} T1w/T1w.nii.gz T2w/T2w.nii.gz --forceqform --forcerun"
-            )
-            print(f"Done running charm on {subject}")
+            #check if m2m exists in this dir
+            if f"m2m_{subject[-4:]}" not in os.listdir():
+                print(f"Running charm on {subject}")
+                os.system(
+                    f"charm {subject[-4:]} T1w/T1w.nii.gz T2w/T2w.nii.gz --forceqform --forcerun"
+                )
+                print(f"Done running charm on {subject}")
 
 
 if __name__ == "__main__":
